137e1fd48a81db81c2915b8ac6622fec6a210aae
NepSauce <greasyandmoist@gmail.com>
2026-02-16 18:57:34 -0400

    Chore: Update to set a custom title and add application logo

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/frames/HAMFrame.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/frames/HAMFrame.java
@@ -1,11 +1,21 @@
 package ham.swing.frames;
 
+import java.io.File;
+
+import javax.swing.ImageIcon;
+
 public class HAMFrame {
     public HAMFrame() {
-        javax.swing.JFrame frame = new javax.swing.JFrame("Fetch-Ham Main Frame");
+        javax.swing.JFrame frame = new javax.swing.JFrame("Fetch-Ham - HTML Asset Map");
         frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null); // Center on screen
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null); 
         frame.setVisible(true);
+
+        File logoFile = new File("media/fetch_ham_logo.png");
+        
+        if (logoFile.isFile()) {
+            frame.setIconImage(new ImageIcon(logoFile.getPath()).getImage());
+        }
     }
 }
