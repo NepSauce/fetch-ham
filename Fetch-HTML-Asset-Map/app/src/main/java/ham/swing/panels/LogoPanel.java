fe0d8257db8820d30f2109b8abe96fbaf964fa8d
NepSauce <greasyandmoist@gmail.com>
2026-02-16 23:10:08 -0400

    Feat: Update LogoPanel to display separate name labels with new styling

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/LogoPanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/LogoPanel.java
@@ -17,7 +17,8 @@ public class LogoPanel{
     private ImageIcon logoIcon;
     private Image scaledImage;
     private JLabel logoLabel;
-    private JLabel nameLabel;
+    private JLabel nameLabel1;
+    private JLabel nameLabel2;
 
     public LogoPanel() {
         headerPanel = new JPanel();
@@ -25,18 +26,24 @@ public class LogoPanel{
         logoIcon = new ImageIcon(logoFile.getPath());
         scaledImage = logoIcon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
         logoLabel = new JLabel(new ImageIcon(scaledImage));
-        nameLabel = new JLabel("Fetch Ham");
+        nameLabel1 = new JLabel("Fetch");
+        nameLabel2 = new JLabel("Ham");
 
-        nameLabel.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 18));
+        nameLabel1.setFont(new java.awt.Font("Montserrat", java.awt.Font.BOLD, 20));
+        nameLabel1.setForeground(new Color(39,39,37, 255));
+        nameLabel2.setFont(new java.awt.Font("Montserrat", java.awt.Font.BOLD, 20));
+        nameLabel2.setForeground(new Color(158,54,84, 255));
         headerPanel.setBackground(Color.WHITE);
         headerPanel.setBounds(5, 5, 150, 40);
         headerPanel.setLayout(null);
 
         logoLabel.setBounds(8, 5, 32, 32);
-        nameLabel.setBounds(48, 5, 100, 32);
+        nameLabel1.setBounds(45, 5, 100, 32);
+        nameLabel2.setBounds(100, 5, 100, 32);
 
         headerPanel.add(logoLabel);
-        headerPanel.add(nameLabel);
+        headerPanel.add(nameLabel1);
+        headerPanel.add(nameLabel2);
 
         headerPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
         
