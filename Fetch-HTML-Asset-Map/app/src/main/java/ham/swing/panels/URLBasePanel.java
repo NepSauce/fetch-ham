dc273a5f0840b3b5e782a2fb21a911fb9164c770
NepSauce <greasyandmoist@gmail.com>
2026-02-17 02:15:08 -0400

    Feat: Add URLComboBox to URLBasePanel for mode selection

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLBasePanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLBasePanel.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 
+import ham.swing.panels.URLPanels.URLComboBox;
+
 @SuppressWarnings("FieldMayBeFinal")
 public class URLBasePanel {
     private JPanel mainPanel;
@@ -12,6 +14,9 @@ public class URLBasePanel {
     public URLBasePanel() {
         mainPanel = new JPanel(new BorderLayout(0, 0));
         mainPanel.setBackground(Color.WHITE); 
+
+        URLComboBox urlComboBox = new URLComboBox();
+        mainPanel.add(urlComboBox.getModeComboBox(), BorderLayout.NORTH);
     }
 
     public JPanel getPanel() {
--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLComboBox.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLComboBox.java
@@ -1,5 +1,20 @@
 package ham.swing.panels.URLPanels;
 
+import javax.swing.JComboBox;
+
 public class URLComboBox {
-    
+    private JComboBox<String> modeComboBox;
+
+    public URLComboBox() {
+        String[] depthFlag = {"Seed", "Contained"};
+
+        modeComboBox = new JComboBox<>(depthFlag);
+        modeComboBox.setSelectedIndex(0); // Default to "Seed"
+        modeComboBox.setPreferredSize(new java.awt.Dimension(50, 45));
+
+    }
+
+    public JComboBox<String> getModeComboBox() {
+        return modeComboBox;
+    }
 }
