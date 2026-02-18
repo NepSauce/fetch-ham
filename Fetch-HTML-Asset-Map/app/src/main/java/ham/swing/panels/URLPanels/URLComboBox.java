5c9cb309a8cee3f2bc9a9fdeca8e22ed22955668
NepSauce <greasyandmoist@gmail.com>
2026-02-17 23:57:44 -0400

    Feat: Enhance URLPanel with font settings and improved dimensions

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLComboBox.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLComboBox.java
@@ -1,11 +1,13 @@
 package ham.swing.panels.URLPanels;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 
-public class URLComboBox {
+@SuppressWarnings("FieldMayBeFinal")
+public class URLComboBox {  
     private JComboBox<String> modeComboBox;
 
     public URLComboBox() {
@@ -14,10 +16,13 @@ public class URLComboBox {
         modeComboBox = new JComboBox<>(depthFlag);
         modeComboBox.setSelectedIndex(0);
         modeComboBox.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 0)); 
-        modeComboBox.setPreferredSize(new java.awt.Dimension(90, 35));
+        modeComboBox.setPreferredSize(new java.awt.Dimension(100, 35));
         modeComboBox.setBackground(Color.WHITE);
-        modeComboBox.setMaximumSize(new java.awt.Dimension(90, 35));
-        modeComboBox.setMinimumSize(new java.awt.Dimension(90, 35));
+        modeComboBox.setMaximumSize(new java.awt.Dimension(100, 35));
+        modeComboBox.setMinimumSize(new java.awt.Dimension(100, 35));
+        
+        Font fontA = new Font("Arial", Font.ROMAN_BASELINE, 16);
+        modeComboBox.setFont(fontA);
     }
 
     public JComboBox<String> getModeComboBox() {
