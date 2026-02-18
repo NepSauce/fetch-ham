82e63f6fc9e0229b207bdc2586e1129e91c19a08
NepSauce <greasyandmoist@gmail.com>
2026-02-17 20:45:01 -0400

    Feat: Add URLPanel class and integrate it into URLBasePanel

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLPanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLPanel.java
@@ -1,5 +1,17 @@
 package ham.swing.panels.URLPanels;
 
+import javax.swing.JTextArea;
+
+@SuppressWarnings("FieldMayBeFinal")
 public class URLPanel {
-    
+    private JTextArea urlTextArea;
+
+    public URLPanel() {
+        urlTextArea = new JTextArea();
+        urlTextArea.setLineWrap(true);
+    }
+
+    public JTextArea getURLPanel() {
+        return urlTextArea;
+    }
 }
