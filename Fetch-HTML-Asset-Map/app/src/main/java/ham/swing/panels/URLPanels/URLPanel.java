58f95680f18f4948bdd3c8bd4edf732bf7bd4aa7
NepSauce <greasyandmoist@gmail.com>
2026-02-18 22:19:04 -0400

    Chore: Refactor URLPanel to use JTextField

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLPanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLPanel.java
@@ -4,30 +4,29 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
 @SuppressWarnings("FieldMayBeFinal")
 public class URLPanel {
-    private JTextArea urlTextArea;
+    private JTextField urlTextArea;
 
     public URLPanel() {
-        urlTextArea = new JTextArea();
-        urlTextArea.setLineWrap(true);
-        urlTextArea.setWrapStyleWord(true);
-        urlTextArea.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 0));
-        urlTextArea.setPreferredSize(new java.awt.Dimension(275, 35));
-        urlTextArea.setMaximumSize(new java.awt.Dimension(275, 35));
+        urlTextArea = new JTextField();
+        urlTextArea.setPreferredSize(new java.awt.Dimension(350, 35));
+        urlTextArea.setMaximumSize(new java.awt.Dimension(350, 35));
         urlTextArea.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
-        urlTextArea.setMinimumSize(new java.awt.Dimension(275, 35));
+        urlTextArea.setMinimumSize(new java.awt.Dimension(350, 35));
         urlTextArea.setBackground(new Color(238,238,238,255));
-        urlTextArea.setSize(new java.awt.Dimension(275, 35));
+        urlTextArea.setSize(new java.awt.Dimension(350, 35));
+        urlTextArea.setEditable(true);
+        urlTextArea.setMargin(new java.awt.Insets(0, 50, 0, 0));
 
         Font fontA = new Font("Arial", Font.PLAIN, 20);
         urlTextArea.setFont(fontA); 
     }
 
-    public JTextArea getURLPanel() {
+    public JTextField getURLPanel() {
         return urlTextArea;
     }
 }
