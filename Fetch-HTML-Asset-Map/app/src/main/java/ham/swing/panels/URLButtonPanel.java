d1c4369633829ca81c5ed7aafd3e2333cb567c48
NepSauce <greasyandmoist@gmail.com>
2026-02-20 00:45:18 -0400

    Chore: Implement URLButtonPanel with Fetch Robots.txt button

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLButtonPanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLButtonPanel.java
@@ -1,5 +1,25 @@
 package ham.swing.panels;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+@SuppressWarnings("FieldMayBeFinal")
 public class URLButtonPanel {
-    
+    private JPanel buttonPanel;
+
+    public URLButtonPanel() {
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JButton robotButton = new JButton("Fetch Robots.txt");
+
+        buttonPanel.add(robotButton);
+    }
+
+    public JPanel getButtonPanel() {
+        return buttonPanel;
+    }
 }
