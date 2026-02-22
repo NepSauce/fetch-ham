c5e8302f789d9bbe43477f1919b5624d0c77766f
NepSauce <greasyandmoist@gmail.com>
2026-02-22 02:00:22 -0400

    Fix: Update URLMapButton layout and button

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLMapButton.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLMapButton.java
@@ -1,7 +1,9 @@
 package ham.swing.panels.URLPanels;
 
 import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -11,15 +13,17 @@ public class URLMapButton {
     public URLMapButton() {
         mapButtonPanel = new JPanel();
 
+        mapButtonPanel.setLayout(new BoxLayout(mapButtonPanel, BoxLayout.X_AXIS));
         mapButtonPanel.setBackground(new java.awt.Color(Color.WHITE.getRGB()));
         mapButtonPanel.setPreferredSize(new java.awt.Dimension(100, 35));
         mapButtonPanel.setMaximumSize(new java.awt.Dimension(100, 35));
         mapButtonPanel.setMinimumSize(new java.awt.Dimension(100, 35));
         
         JButton mapButton = new JButton("Map");
-        mapButton.setPreferredSize(new java.awt.Dimension(100, 17));
-        mapButton.setMaximumSize(new java.awt.Dimension(100, 17));
-        mapButton.setMinimumSize(new java.awt.Dimension(100, 17));  
+        mapButton.setPreferredSize(new java.awt.Dimension(100, 35));
+        mapButton.setMaximumSize(new java.awt.Dimension(100, 35));
+        mapButton.setMinimumSize(new java.awt.Dimension(100, 35));  
+        mapButton.setAlignmentY(Component.CENTER_ALIGNMENT);
 
         mapButtonPanel.add(mapButton);  
     }
