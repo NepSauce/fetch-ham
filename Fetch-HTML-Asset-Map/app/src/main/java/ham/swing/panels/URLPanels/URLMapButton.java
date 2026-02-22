af1c10faffbdb5e7afe71656013ca9d1173b08b9
NepSauce <greasyandmoist@gmail.com>
2026-02-22 01:54:17 -0400

    Feat: Add URLMapButton

---
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLMapButton.java
@@ -0,0 +1,26 @@
+package ham.swing.panels.URLPanels;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class URLMapButton {
+    JPanel mapButtonPanel;
+
+    public URLMapButton() {
+        mapButtonPanel = new JPanel();
+
+        mapButtonPanel.setBackground(new java.awt.Color(Color.WHITE.getRGB()));
+        mapButtonPanel.setPreferredSize(new java.awt.Dimension(100, 35));
+        mapButtonPanel.setMaximumSize(new java.awt.Dimension(100, 35));
+        mapButtonPanel.setMinimumSize(new java.awt.Dimension(100, 35));
+        
+        JButton mapButton = new JButton("Map");
+        mapButton.setPreferredSize(new java.awt.Dimension(100, 17));
+        mapButton.setMaximumSize(new java.awt.Dimension(100, 17));
+        mapButton.setMinimumSize(new java.awt.Dimension(100, 17));  
+        mapButtonPanel.add(mapButton);  
+    }
+    
+}
