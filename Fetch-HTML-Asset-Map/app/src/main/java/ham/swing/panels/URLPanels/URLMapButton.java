b5fd1a4a5d4cfd403284545ea5e3adcc5159f9ae
NepSauce <greasyandmoist@gmail.com>
2026-02-22 02:02:09 -0400

    Fix: Simplify background color setting for URLMapButton

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLMapButton.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLMapButton.java
@@ -14,16 +14,18 @@ public class URLMapButton {
         mapButtonPanel = new JPanel();
 
         mapButtonPanel.setLayout(new BoxLayout(mapButtonPanel, BoxLayout.X_AXIS));
-        mapButtonPanel.setBackground(new java.awt.Color(Color.WHITE.getRGB()));
+        mapButtonPanel.setBackground(Color.WHITE);
         mapButtonPanel.setPreferredSize(new java.awt.Dimension(100, 35));
         mapButtonPanel.setMaximumSize(new java.awt.Dimension(100, 35));
         mapButtonPanel.setMinimumSize(new java.awt.Dimension(100, 35));
+        mapButtonPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
         
         JButton mapButton = new JButton("Map");
         mapButton.setPreferredSize(new java.awt.Dimension(100, 35));
         mapButton.setMaximumSize(new java.awt.Dimension(100, 35));
         mapButton.setMinimumSize(new java.awt.Dimension(100, 35));  
         mapButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+
 
         mapButtonPanel.add(mapButton);  
     }
