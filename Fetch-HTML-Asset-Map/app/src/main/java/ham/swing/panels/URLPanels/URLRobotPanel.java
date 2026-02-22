3245692f273e07f481f8a32582406c83ac01e20f
NepSauce <greasyandmoist@gmail.com>
2026-02-22 01:22:32 -0400

    Feat: Update URLRobotPanel layout to BoxLayout and enhance button styling

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLRobotPanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLPanels/URLRobotPanel.java
@@ -1,7 +1,10 @@
 package ham.swing.panels.URLPanels;
 
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -10,10 +13,18 @@ public class URLRobotPanel {
 
     public URLRobotPanel() {
         buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(2, 1));
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
 
         JButton robotButton = new JButton("robots.txt");
+        robotButton.setPreferredSize(new Dimension(70, 17));
+        robotButton.setMaximumSize(new Dimension(70, 17));
+        robotButton.setMinimumSize(new Dimension(70, 17));
+        robotButton.setBorder(BorderFactory.createLineBorder(new Color(124, 124, 124, 255), 1));
         JButton viewRulesButton = new JButton("View rules");
+        viewRulesButton.setPreferredSize(new Dimension(70, 17));
+        viewRulesButton.setMaximumSize(new Dimension(70, 17));
+        viewRulesButton.setMinimumSize(new Dimension(70, 17));
+        viewRulesButton.setBorder(BorderFactory.createLineBorder(new Color(124, 124, 124, 255), 1));
 
         buttonPanel.add(robotButton);
         buttonPanel.add(viewRulesButton);
