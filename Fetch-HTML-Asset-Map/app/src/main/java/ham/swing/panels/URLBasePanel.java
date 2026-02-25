443f5cfa27bb1de4d698ea85ec595dfad5643bc1
NepSauce <greasyandmoist@gmail.com>
2026-02-24 23:08:18 -0400

    Frontend&Refactor: Expose URLBasePanel controls for controller wiring

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLBasePanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLBasePanel.java
@@ -5,15 +5,21 @@ import java.awt.Component;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
 import ham.swing.panels.URLPanels.URLComboBox;
 import ham.swing.panels.URLPanels.URLPanel;
+import ham.swing.panels.URLPanels.URLSwitchPanel;
 
 @SuppressWarnings("FieldMayBeFinal")
 public class URLBasePanel {
     private JPanel mainPanel;
+    private URLComboBox urlComboBox;
+    private URLPanel urlPanel;
+    private URLButtonPanel urlButtonPanel;
 
     public URLBasePanel() {
         mainPanel = new JPanel();
@@ -21,20 +27,36 @@ public class URLBasePanel {
         mainPanel.setBackground(Color.WHITE); 
         mainPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
 
-        URLComboBox urlComboBox = new URLComboBox();
+        urlComboBox = new URLComboBox();
         urlComboBox.getModeComboBox().setAlignmentY(Component.CENTER_ALIGNMENT);
         mainPanel.add(urlComboBox.getModeComboBox());
 
-        URLPanel urlPanel = new URLPanel();
+        urlPanel = new URLPanel();
         urlPanel.getURLPanel().setAlignmentY(Component.CENTER_ALIGNMENT);
         mainPanel.add(urlPanel.getURLPanel());
 
-        URLButtonPanel urlButtonPanel = new URLButtonPanel();
+        urlButtonPanel = new URLButtonPanel();
         urlButtonPanel.getMainControlPanel().setAlignmentY(Component.CENTER_ALIGNMENT);
         mainPanel.add(urlButtonPanel.getMainControlPanel());
     }
 
     public JPanel getPanel() {
         return mainPanel;
+    }
+
+    public JComboBox<String> getModeComboBox() {
+        return urlComboBox.getModeComboBox();
+    }
+
+    public JTextField getUrlField() {
+        return urlPanel.getURLPanel();
+    }
+
+    public URLButtonPanel getUrlButtonPanel() {
+        return urlButtonPanel;
+    }
+
+    public URLSwitchPanel getUrlSwitchPanel() {
+        return urlButtonPanel.getUrlSwitchPanel();
     }
 }
