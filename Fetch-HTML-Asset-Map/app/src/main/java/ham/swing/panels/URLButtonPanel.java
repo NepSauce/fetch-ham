ce4e231ce1efe043c480f1b4a1ed0fd444511a48
NepSauce <greasyandmoist@gmail.com>
2026-02-24 23:08:18 -0400

    Frontend&Refactor: Add URLButtonPanel stop control and getters

--- Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLButtonPanel.java
+++ Fetch-HTML-Asset-Map/app/src/main/java/ham/swing/panels/URLButtonPanel.java
@@ -3,15 +3,20 @@ package ham.swing.panels;
 import java.awt.Color;
 import java.awt.Component;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 import ham.swing.panels.URLPanels.URLMapButton;
+import ham.swing.panels.URLPanels.URLStopButton;
 import ham.swing.panels.URLPanels.URLSwitchPanel;
 
 @SuppressWarnings("FieldMayBeFinal")
 public class URLButtonPanel {
     private JPanel mainControlPanel;
+    private URLMapButton urlMapButton;
+    private URLSwitchPanel urlSwitchPanel;
+    private URLStopButton urlStopButton;
 
     public URLButtonPanel() {
         mainControlPanel = new JPanel();
@@ -19,16 +24,34 @@ public class URLButtonPanel {
         mainControlPanel.setBackground(Color.WHITE);
         mainControlPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
 
-        URLMapButton urlMapButton = new URLMapButton();
+        urlMapButton = new URLMapButton();
         urlMapButton.getURLMapButtonPanel().setAlignmentY(Component.CENTER_ALIGNMENT);
         mainControlPanel.add(urlMapButton.getURLMapButtonPanel());
 
-        URLSwitchPanel urlSwitchPanel = new URLSwitchPanel();
+        urlSwitchPanel = new URLSwitchPanel();
         urlSwitchPanel.getSwitchPanel().setAlignmentY(Component.CENTER_ALIGNMENT);
         mainControlPanel.add(urlSwitchPanel.getSwitchPanel());
+
+        mainControlPanel.add(Box.createHorizontalGlue());
+
+        urlStopButton = new URLStopButton();
+        urlStopButton.getStopButtonPanel().setAlignmentY(Component.CENTER_ALIGNMENT);
+        mainControlPanel.add(urlStopButton.getStopButtonPanel());
     }
 
     public JPanel getMainControlPanel() {
         return mainControlPanel;
+    }
+
+    public URLMapButton getUrlMapButton() {
+        return urlMapButton;
+    }
+
+    public URLSwitchPanel getUrlSwitchPanel() {
+        return urlSwitchPanel;
+    }
+
+    public URLStopButton getUrlStopButton() {
+        return urlStopButton;
     }
 }
